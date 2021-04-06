02149d07e314804ff314124fd4630c9ed3df296e
OleksTheSecond <80333138+OleksTheSecond@users.noreply.github.com>
2021-04-06 20:58:53 +0300

    Update Dao.java

--- java-project-conf/src/main/java/com/my/dao/Dao.java
+++ java-project-conf/src/main/java/com/my/dao/Dao.java
@@ -1,11 +1,12 @@
 package com.my.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface Dao<T> {
-     boolean create(T obj);
-     boolean update(T obj);
-     boolean delete(T obj);
-     T findById(int id);
-     List<T> findAll();
+     boolean create(T obj) throws SQLException;
+     boolean update(T obj) throws SQLException;
+     boolean delete(T obj) throws SQLException;
+     T findById(int id) throws SQLException;
+     List<T> findAll() throws SQLException;
 }
