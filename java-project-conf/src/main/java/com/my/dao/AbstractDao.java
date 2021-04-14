621f7f6e234d61f73d2cb1e970d447eb49736967
OleksTheSecond <80333138+OleksTheSecond@users.noreply.github.com>
2021-04-14 14:47:40 +0300

    Update AbstractDao.java

--- java-project-conf/src/main/java/com/my/dao/AbstractDao.java
+++ java-project-conf/src/main/java/com/my/dao/AbstractDao.java
@@ -1,22 +1,92 @@
 package com.my.dao;
 
-import org.apache.commons.dbcp.BasicDataSource;
+import com.my.domain.Conference;
+import com.my.domain.Entity;
+import org.h2.jdbcx.JdbcConnectionPool;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public abstract class AbstractDao implements Dao {
-    private static BasicDataSource ds = new BasicDataSource();
-    static {
-        ds.setUrl("jdbc:h2:~/conf");
-        ds.setUsername("sa");
-        ds.setPassword("sa");
-        ds.setMinIdle(5);
-        ds.setMaxIdle(10);
-        ds.setMaxOpenPreparedStatements(100);
-    }
+public abstract class AbstractDao<E extends Entity> implements Dao<E> {
+    JdbcConnectionPool cp = JdbcConnectionPool.create(
+            "jdbc:h2:~/conf", "sa", "sa");
+
+
 
     public Connection getConnection() throws SQLException {
-        return ds.getConnection();
+        return cp.getConnection();
+    }
+
+    protected abstract String getUpdateSql();
+
+    protected abstract String getDeleteSql();
+
+    protected abstract String getCreateSql();
+
+    protected abstract String getFindByIdSql();
+
+    protected abstract String getFindAllSql();
+
+    protected abstract EntityMapper<E> getEntityMapper() throws SQLException;
+
+    @Override
+    public E findById(Long id) throws SQLException{
+        try (Connection con = getConnection(); PreparedStatement st = con.prepareStatement(getFindByIdSql())) {
+            st.setLong(1, id);
+            ResultSet resultSet = st.executeQuery();
+            if (resultSet.next()) {
+                EntityMapper<E> entityMapper = getEntityMapper();
+                return entityMapper.fromResultSet(resultSet);
+            }
+        }
+        return null;
+    }
+    @Override
+    public boolean deleteById(Long id) throws SQLException {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(getDeleteSql())) {
+            preparedStatement.setLong(1,id);
+            preparedStatement.execute();
+            return true;
+        }
+    }
+
+    @Override
+    public List<E> findAll() throws SQLException {
+        try (Connection con = getConnection();
+             Statement st  = con.createStatement();
+             ResultSet resultSet = st.executeQuery(getFindAllSql())) {
+
+            List<E> array = new ArrayList<>();
+            while (resultSet.next()) {
+                EntityMapper<E> entityMapper = getEntityMapper();
+                array.add(entityMapper.fromResultSet(resultSet));
+            }
+            return array;
+        }
+    }
+
+    @Override
+    public boolean create(E entity) throws SQLException {
+        try (Connection con = getConnection();
+             PreparedStatement st = con.prepareStatement(getCreateSql(),
+                     Statement.RETURN_GENERATED_KEYS)) {
+
+            getEntityMapper().fillCreateStatement(st, entity);
+            st.executeUpdate();
+            return true;
+        }
+    }
+
+    @Override
+    public boolean update(E entity) throws SQLException {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(getUpdateSql())) {
+
+            getEntityMapper().fillUpdateStatement(preparedStatement, entity);
+            preparedStatement.executeUpdate();
+            return true;
+        }
     }
 }
