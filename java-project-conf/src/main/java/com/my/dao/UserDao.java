7091e06a212d343f1cdcf3a06f5d8b25908bb81d
OleksTheSecond <80333138+OleksTheSecond@users.noreply.github.com>
2021-04-14 14:50:39 +0300

    Update UserDao.java

--- java-project-conf/src/main/java/com/my/dao/UserDao.java
+++ java-project-conf/src/main/java/com/my/dao/UserDao.java
@@ -7,100 +7,67 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserDao extends AbstractDao {
+public class UserDao extends AbstractDao<User>{
 
-    public boolean create(Object obj) throws SQLException {
-        User user = (User) obj;
-
-        try (Connection connection = getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(UserDaoConstants.INSERT_USER)){
-
-            preparedStatement.setInt(1, user.getUserId());
-            preparedStatement.setString(2, user.getFirstName());
-            preparedStatement.setString(3, user.getLastName());
-            preparedStatement.setInt(4, user.getRoleId());
-            preparedStatement.execute();
-            return true;
-        }
-        catch (SQLException ex) {
-           throw new SQLException("UserDao#create");
-        }
-
+    @Override
+    protected String getFindByIdSql() {
+        return UserDaoConstants.FIND_USER;
     }
 
-    public boolean update(Object obj) throws SQLException {
-        User user = (User) obj;
-        try (Connection connection = getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(UserDaoConstants.UPDATE_USER)){
-
-            preparedStatement.setString(1, user.getFirstName());
-            preparedStatement.setString(2, user.getLastName());
-            preparedStatement.setInt(3, user.getRoleId());
-            preparedStatement.setInt(4, user.getUserId());
-            preparedStatement.execute();
-            return true;
-        }
-        catch (SQLException e) {
-            throw new SQLException("UserDao#update");
-        }
-
+    @Override
+    protected String getCreateSql() {
+        return UserDaoConstants.INSERT_USER;
     }
 
-    public boolean delete(Object obj) throws SQLException {
-        int id = ((User) obj).getUserId();
-
-        try (Connection connection = getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(UserDaoConstants.DELETE_USER)){
-
-            preparedStatement.setInt(1, id);
-            preparedStatement.execute();
-            return true;
-        } catch (SQLException e) {
-            throw new SQLException("UserDao#delete");
-        }
-
+    @Override
+    protected String getUpdateSql() {
+        return UserDaoConstants.UPDATE_USER;
     }
 
-    public Object findById(int id) throws SQLException {
-        ResultSet resultSet = null;
-        try (Connection connection = getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(UserDaoConstants.FIND_USER)){
-
-            preparedStatement.setInt(1, id);
-            resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-            return mapUser(resultSet);
-        }
-        catch (SQLException e) {
-            throw new SQLException("UserDao#findById");
-        }
-        finally {
-            resultSet.close();
-        }
-
+    @Override
+    protected String getDeleteSql() {
+        return UserDaoConstants.DELETE_USER;
     }
 
-    public List findAll() throws SQLException {
-        List<User> userList = new ArrayList<>();
-        try (Connection connection = getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(UserDaoConstants.FIND_ALL)) {
+    @Override
+    protected String getFindAllSql() {
+        return UserDaoConstants.FIND_ALL;
+    }
 
-            while (resultSet.next()) {
-                userList.add(mapUser(resultSet));
+
+    @Override
+    protected EntityMapper<User> getEntityMapper() throws SQLException {
+        return new EntityMapper<User>() {
+
+            @Override
+            public User fromResultSet(ResultSet resultSet) throws SQLException {
+                return mapUser(resultSet);
             }
-            return userList;
-        }
-        catch (SQLException e) {
-            throw new SQLException("UserDao#findAll");
-        }
 
+            @Override
+            public void fillCreateStatement(PreparedStatement statement,
+                                            User entity) throws SQLException {
+                statement = fillStatement(statement,entity);
+            }
 
+            @Override
+            public void fillUpdateStatement(PreparedStatement statement, User entity) throws SQLException {
+                statement = fillStatement(statement,entity);
+                statement.setLong(4, entity.getId());
+            }
+
+            private PreparedStatement fillStatement(PreparedStatement st, User entity) throws SQLException {
+                st.setString(1, entity.getFirstName());
+                st.setString(2, entity.getLastName());
+                st.setInt(3, entity.getRoleId());
+                return st;
+            }
+        };
     }
 
     private User mapUser(ResultSet resultSet) throws SQLException {
         try {
-            User user = new User(resultSet.getInt(UserDaoConstants.USER_ID),
+            User user = new User(resultSet.getLong(UserDaoConstants.USER_ID),
                                  resultSet.getString(UserDaoConstants.FIRST_NAME),
                                  resultSet.getString(UserDaoConstants.LAST_NAME),
                                  resultSet.getInt(UserDaoConstants.ROLE_ID));
