f09f9c51480b0c5df3268edf36982b06a04a8d1f
OleksTheSecond <80333138+OleksTheSecond@users.noreply.github.com>
2021-05-25 14:58:27 +0300

    Delete java-spring-conf directory

--- java-spring-conf/src/main/java/com/my/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.my.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan({"com.my"})
-public class AppConfig {
-}
